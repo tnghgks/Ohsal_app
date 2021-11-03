46e57a6f1bdc9760c08f0f533991e5278f54ec53
HosuHan <tnghgks@naver.com>
2021-11-03 15:19:26 +0900

    11/02

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useHistory } from "react-router-dom";
+import styled from "styled-components";
 import axios from "axios";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faBars,
+  faHome,
+  faCalendarAlt,
+  faDrumstickBite,
+  faSignOutAlt,
+} from "@fortawesome/free-solid-svg-icons";
+
+const Nav = styled.nav`
+  width: 50px;
+  height: 100vh;
+  position: fixed;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  font-size: 20px;
+  background-color: brown;
+  color: #ffeadb;
+  padding-top: 10px;
+`;
+
+const Avatar = styled.img`
+  margin-top: 15px;
+  width: 40px;
+  height: 40px;
+  border-radius: 25px;
+`;
+
+const Menu = styled.ul`
+  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const MenuButton = styled(Link)`
+  text-align: center;
+  line-height: 40px;
+  width: 40px;
+  height: 40px;
+  border-radius: 10px;
+  background-color: ${(props) => (props.current ? "#000" : "#fff")};
+`;
+const Logout = styled.a`
+  margin-top: 20px;
+`;
 
 const Header = ({ authenticate, setAuth }) => {
   const history = useHistory();
+  const [path, setPath] = useState(history.location.pathname);
+
+  useEffect(() => {
+    console.log(history);
+    setPath(history.location.pathname);
+  }, [history.location.pathname]);
+
   const handleClick = async (event) => {
     event.preventDefault();
     try {
@@ -18,26 +72,32 @@ const Header = ({ authenticate, setAuth }) => {
   };
 
   return (
-    <nav>
-      <ul>
-        <Link to="/">
-          <li>홈</li>
-        </Link>
-        <Link to="/event">
-          <li>이벤트</li>
-        </Link>
-        <Link to="/battle">
-          <li>내전안내</li>
-        </Link>
-        {authenticate && authenticate ? (
-          <a href="/" onClick={handleClick}>
-            로그아웃
-          </a>
-        ) : (
-          ""
-        )}
-      </ul>
-    </nav>
+    <Nav>
+      <FontAwesomeIcon icon={faBars} />
+      <Avatar
+        id="userAvatar"
+        src={`https://cdn.discordapp.com/avatars/${authenticate.discordId}/${authenticate.avatar}.png`}
+        alt={authenticate.avatar}
+      />
+      {authenticate && authenticate ? (
+        <Logout href="/" onClick={handleClick}>
+          <FontAwesomeIcon icon={faSignOutAlt} />
+        </Logout>
+      ) : (
+        ""
+      )}
+      <Menu>
+        <MenuButton to="/" current={path && path === "/"}>
+          <FontAwesomeIcon icon={faHome} />
+        </MenuButton>
+        <MenuButton to="/event" current={path && path === "/event"}>
+          <FontAwesomeIcon icon={faCalendarAlt} />
+        </MenuButton>
+        <MenuButton to="/battle" current={path && path === "/battle"}>
+          <FontAwesomeIcon icon={faDrumstickBite} />
+        </MenuButton>
+      </Menu>
+    </Nav>
   );
 };
 
--- src/Pages/Battle.js
+++ src/Pages/Battle.js
@@ -1,7 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 100vh;
+  background-color: #637373;
+`;
+
+const H1 = styled.h1``;
 
 const Battle = () => {
-  return <h1>Battle</h1>;
+  return (
+    <Container>
+      <H1>Battle</H1>
+    </Container>
+  );
 };
 
 export default Battle;
